4e28ea2818239a1667d4edf989e9b891542defc3
agong324 <156977729+agong324@users.noreply.github.com>
2024-10-20 14:37:38 -0400

    Update: using -n tag for mv command

--- rename.sh
+++ rename.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# made to rename files according to the jellyfin's episode naming scheme of 'Series Name A S01E01.mkv' et cetera. change Name to the series name.
+# made to rename files according to the jellyfins episode naming scheme of Series Name A S01E01.mkv et cetera. change Name to the series name.
 #or just use this to rename files in general.
 a=1
 for i in *.mkv; do
   if [ "$a" -lt 10 ]; then
-    new=$(printf "Name0%d.mkv" "$a")  # For numbers less than 10, use a leading zero.
+    new=$(printf "Name of Series S01E0%d.mkv" "$a")  # For numbers less than 10, use a leading zero.
   else
-    new=$(printf "Name%d.mkv" "$a")   # For numbers 10 and above, no leading zero.
+    new=$(printf "Name of Series S01E%d.mkv" "$a")   # For numbers 10 and above, no leading zero.
   fi
-  mv -i -- "$i" "$new"
+  mv -n "$i" "$new"
   let a=a+1
 done
