4c2e2f73adb1700d5d0d37752666c2835a234726
agong324 <156977729+agong324@users.noreply.github.com>
2024-10-20 20:03:13 -0400

    Update to take user input instead

--- rename.sh
+++ rename.sh
@@ -2,12 +2,15 @@
 
 # made to rename files according to the jellyfins episode naming scheme of Series Name A S01E01.mkv et cetera. change Name to the series name.
 #or just use this to rename files in general.
-a=1
+read -p "Enter the Show's Name: " n
+read -p "Enter the Season Number: " m
+read -p "Enter the Starting Episode Number: " a
+
 for i in *.mkv; do
   if [ "$a" -lt 10 ]; then
-    new=$(printf "Name of Series S01E0%d.mkv" "$a")  # For numbers less than 10, use a leading zero.
+    new=$(printf "$n S0${m}E0%d.mkv" "$a")  # For numbers less than 10, use a leading zero.
   else
-    new=$(printf "Name of Series S01E%d.mkv" "$a")   # For numbers 10 and above, no leading zero.
+    new=$(printf "$n S0${m}E%d.mkv" "$a")   # For numbers 10 and above, no leading zero.
   fi
   mv -n "$i" "$new"
   let a=a+1
