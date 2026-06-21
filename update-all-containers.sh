7d6096feb9525749de021fe1175030e5d0fe3874
agong324 <156977729+agong324@users.noreply.github.com>
2026-06-21 17:21:21 -0400

    Update update-all-containers.sh

--- update-all-containers.sh
+++ update-all-containers.sh
@@ -2,35 +2,34 @@
 # update all containers within proxmox; starts stopped containers and shuts them off after its done updating too.
 # original script was taken from https://forum.proxmox.com/threads/update-all-lxc-with-one-simple-script.58729/
 
-currTime=$(date)
-echo "$currTime" | tee -a /root/update-log
+currTime=$(date '+%Y-%m-%d_%H:%M:%S')
+mkdir -p /root/update-logs
+LOGS=/root/update-logs/update-$currTime.log
 
 # list of container ids we need to iterate through
 containers=$(pct list | tail -n +2 | cut -f1 -d' ')
 
 function update_container() {
   container=$1
-  name=`pct exec $container cat /etc/hostname`
-  echo "[Info] Updating $container : $name" | tee -a /root/update-log
+  name=$(pct exec "$container" cat /etc/hostname)
+  echo "[Info] Updating $container : $name $currTime" | tee -a "$LOGS"
   # to chain commands within one exec we will need to wrap them in bash
-  pct exec $container -- bash -c "apt update && apt upgrade -y && apt full-upgrade -y && apt autoremove -y"
+  pct exec "$container" -- bash -c "apt update && apt upgrade -y && apt full-upgrade -y && apt autoremove -y"
 }
 
 for container in $containers
 do
-  status=`pct status $container`
+  status=$(pct status "$container")
   if [ "$status" == "status: stopped" ]; then
-    echo "[Info] Starting $container : $name" | tee -a /root/update-log
-    pct start $container
+    echo "[Info] Starting $container : $name $currTime" | tee -a "$LOGS"
+    pct start "$container"
     echo "[Info] Sleeping 5 seconds"
     sleep 5
-    update_container $container
-    echo "[Info] Shutting down $container : $name" | tee -a /root/update-log
-    pct shutdown $container &
+    update_container "$container"
+    echo "[Info] Shutting down $container : $name $currTime" | tee -a "$LOGS"
+    pct shutdown "$container" &
   elif [ "$status" == "status: running" ]; then
-    echo "[Info] $container : $name already running" | tee -a /root/update-log
-    update_container $container
+    echo "[Info] $container : $name already running $currTime" | tee -a "$LOGS"
+    update_container "$container"
   fi
 done; wait
-
-cat /root/update-log | less
