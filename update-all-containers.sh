f2b721dd999872039aeb43c8ac31d873e61dfbbe
agong324 <156977729+agong324@users.noreply.github.com>
2026-07-25 23:52:52 -0400

    Update update-all-containers.sh
    changed pct to /usr/sbin/pct

--- update-all-containers.sh
+++ update-all-containers.sh
@@ -7,27 +7,27 @@ mkdir -p /root/update-logs
 LOGS=/root/update-logs/update-$currTime.log
 
 # list of container ids we need to iterate through
-containers=$(pct list | tail -n +2 | cut -f1 -d' ')
+containers=$(/usr/sbin/pct list | tail -n +2 | cut -f1 -d' ')
 
 function update_container() {
   container=$1
-  name=$(pct exec "$container" cat /etc/hostname)
+  name=$(/usr/sbin/pct exec "$container" cat /etc/hostname)
   echo "[Info] Updating $container : $name $currTime" | tee -a "$LOGS"
   # to chain commands within one exec we will need to wrap them in bash
-  pct exec "$container" -- bash -c "apt update && apt upgrade -y && apt full-upgrade -y && apt autoremove -y"
+  /usr/sbin/pct exec "$container" -- bash -c "apt update && apt upgrade -y && apt full-upgrade -y && apt autoremove -y"
 }
 
 for container in $containers
 do
-  status=$(pct status "$container")
+  status=$(/usr/sbin/pct status "$container")
   if [ "$status" == "status: stopped" ]; then
     echo "[Info] Starting $container : $name $currTime" | tee -a "$LOGS"
-    pct start "$container"
+    /usr/sbin/pct start "$container"
     echo "[Info] Sleeping 5 seconds"
     sleep 5
     update_container "$container"
     echo "[Info] Shutting down $container : $name $currTime" | tee -a "$LOGS"
-    pct shutdown "$container" &
+    /usr/sbin/pct shutdown "$container" &
   elif [ "$status" == "status: running" ]; then
     echo "[Info] $container : $name already running $currTime" | tee -a "$LOGS"
     update_container "$container"
